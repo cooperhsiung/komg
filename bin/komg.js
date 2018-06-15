2f0526ed8a192d2e279425cab7112e7601c3cebf
Cooper Hsiung <cooperhsiung@gmail.com>
2018-06-15 17:23:56 +0800

    improve db path

--- bin/komg.js
+++ bin/komg.js
@@ -4,12 +4,14 @@ const program = require('commander');
 program
   .version(require('../package.json').version)
   .option('-p, --port <p>', 'set port, default 2350')
+  .option('--db <db>', 'set db path, default $komg/lib/db.json')
   .option('--env <env>', 'set NODE_ENV, default dev, another option prod')
   .option('--redis <redis>', 'redis url, default redis://127.0.0.1:6379/0')
   .option('--basic-auth <basic-auth>', 'set basic auth, default none, eg:user1=password1')
   .parse(process.argv);
 
 process.env.PORT = program.port || 2350;
+process.env.DB = program.db || 'db.json';
 process.env.NODE_ENV = program.env || 'dev';
 process.env.REDIS = program.redis || 'redis://127.0.0.1:6379/0';
 
